e120709158125e42e23b13b4639c5bb8d8e25646
Joey <smithjoey5123@gmail.com>
2018-08-24 13:52:09 -0500

    hashset exercise

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> showRooms = new HashSet<string>();
+
+            showRooms.Add("Tesla");
+            showRooms.Add("Porsche");
+            showRooms.Add("Alfa Romeo");
+            showRooms.Add("Chevrolet");
+            showRooms.Add("Tesla");
+
+            HashSet<string> usedLot = new HashSet<string>();
+
+            usedLot.Add("BMW");
+            usedLot.Add("Toyota");
+
+            showRooms.UnionWith(usedLot);
+
+            showRooms.Remove("BMW");
+
+            HashSet<string> Junkyard = new HashSet<string>();
+            
+            Junkyard.Add("Chevrolet");
+            Junkyard.Add("Ford");
+            Junkyard.Add("Nissan");
+            Junkyard.Add("Honda");
+            Junkyard.Add("Alfa Romeo");
+
+            HashSet<string> clone = new HashSet<string>(showRooms);
+
+            clone.IntersectWith(showRooms);
+
+            showRooms.UnionWith(Junkyard);
+
+            showRooms.Remove("Ford");
+
+            System.Console.WriteLine(showRooms.Count);
+        }
+    }
+}
